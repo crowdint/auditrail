fb599d24ea28b75d66ca96113e7661132c6da403
Luis Galaviz <luis.galaviz@crowdint.com>
2010-12-06 17:11:38 -0600

    Migrations specs.

--- spec/migrations_spec.rb
+++ spec/migrations_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 require 'generators/migrations'
+require 'fileutils'
 
 describe Auditrail::Generators::MigrationsGenerator do
-  subject { Auditrail::Generators::MigrationsGenerator }
-  let(:destination) { File.expand_path("spec/tmp") }
+  context "migration" do
+    subject { Auditrail::Generators::MigrationsGenerator }
+    let(:destination) { File.expand_path("spec/tmp") }
   
-  before do
-    subject.start([] , :destination_root => destination)
-  end
+    before do
+      subject.start([] , :destination_root => destination)
+    end
+  
+    after do
+      FileUtils.rm_rf(destination)
+    end
 
-  it "should generate a migration" do
-    
+    it "blah" do
+      Dir.glob(File.join(File.expand_path("#{destination}/db/migrate"), "*_create_auditrails.rb")).should have_exactly(1).items
+    end
   end
 
 end
